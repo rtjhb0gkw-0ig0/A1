a50b9b107dab9404f0a4aaa854850762e44dd72a
rtjhb0gkw-0ig0 <151805344+rtjhb0gkw-0ig0@users.noreply.github.com>
2024-01-04 10:44:40 +0800

    Add files via upload

--- A1.py
+++ A1.py
@@ -1,5 +1,4 @@
 class FireChicken:
-    
     def __init__(self, a1, a2, a3, a4, a5):#初始化數值
         self.a1 = a1
         self.a2 = a2
@@ -17,11 +16,29 @@ class FireChicken:
         self.a5 = a5
         print("SF")    
     def fn3(self):#輸出所有數值
-        print(self.a1, self.a2, self.a3, self.a4, self.a5)  
+        print(self.a1, self.a2, self.a3, self.a4, self.a5) 
 
-
-g_FireChicken = FireChicken(1,2,3,4,5)
-g_FireChicken.fn3()#輸出所有數值
-g_FireChicken.fn1(10,12)#設定a1~a2的數值
-g_FireChicken.fn2(24,32,44)#設定a3~a5的數值
-g_FireChicken.fn3()#輸出所有數值
+print("g_FireChicken_1:")
+g_FireChicken_1 = FireChicken(1,2,3,4,5)
+g_FireChicken_1.fn3()#輸出所有數值
+g_FireChicken_1.fn1(10,12)#設定a1~a2的數值
+g_FireChicken_1.fn2(24,32,44)#設定a3~a5的數值
+g_FireChicken_1.fn3()#輸出所有數值
+print("g_FireChicken_2:")
+g_FireChicken_2 = FireChicken(5,4,3,2,1)
+g_FireChicken_2.fn3()#輸出所有數值
+g_FireChicken_2.fn1(151,141)#設定a1~a2的數值
+g_FireChicken_2.fn2(354,456,985)#設定a3~a5的數值
+g_FireChicken_2.fn3()#輸出所有數值
+print("g_FireChicken_3:")
+g_FireChicken_3 = FireChicken(5,4,3,2,1)
+g_FireChicken_3.fn3()#輸出所有數值
+g_FireChicken_3.fn1(884,635)#設定a1~a2的數值
+g_FireChicken_3.fn2(125,25,35)#設定a3~a5的數值
+g_FireChicken_3.fn3()#輸出所有數值
+print("g_FireChicken_4:")
+g_FireChicken_4 = FireChicken(10,22,46,35,51)
+g_FireChicken_4.fn3()#輸出所有數值
+g_FireChicken_4.fn1(58,14)#設定a1~a2的數值
+g_FireChicken_4.fn2(54,75,25)#設定a3~a5的數值
+g_FireChicken_4.fn3()#輸出所有數值
